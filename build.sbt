7f09921751a2e4785d4100605a9e12a930e57be9
Rahul Goyal <rahulg@qubole.com>
2020-01-14 23:58:35 +0530

    Word Count Example

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ name := "spark-scala-examples"
 version := "0.1"
 
 scalaVersion := "2.11.12"
+
+val sparkVersion = "2.4.3"
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion)
--- src/main/scala/com/rahulg/bootcamp/WordCount.scala
+++ src/main/scala/com/rahulg/bootcamp/WordCount.scala
@@ -1,9 +1,25 @@
 package com.rahulg.bootcamp
 
+import org.apache.spark.{SparkConf, SparkContext}
+
 object WordCount {
 
   def main(array: Array[String]): Unit = {
     println("This program will calculate the number of words present in a file.")
+
+    val master = "local"
+    val conf = new SparkConf().setAppName("WordCountExample").setMaster(master)
+    val sc  = new SparkContext(conf)
+    val fileName = "file:/Users/rahulg/learning/bigdata/spark/scala/spark-scala-examples/data/InputData.txt"
+
+    val fileRdd = sc.textFile(fileName)
+
+    val wordsRdd = fileRdd.flatMap(lines => lines.split(" ")).map(words => (words, 1)).reduceByKey((a, b) => a + b)
+
+    val wordCount = wordsRdd.count()
+
+    System.out.println("Result  of word Count: " + wordCount)
+    sc.stop()
   }
 
 }
