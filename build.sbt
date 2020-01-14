2903a17696ef42728b3d80c45a6beb38913da220
Rahul Goyal <rahulg@qubole.com>
2020-01-14 23:00:01 +0530

    Hello World

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "spark-scala-examples"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
---
+++ src/main/scala/com/rahulg/bootcamp/WordCount.scala
@@ -0,0 +1,9 @@
+package com.rahulg.bootcamp
+
+object WordCount {
+
+  def main(array: Array[String]): Unit = {
+    println("This program will calculate the number of words present in a file.")
+  }
+
+}
